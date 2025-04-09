78a12cf4c0f5567396e16f69b2a6c08d8a9e3109
domyeong <wjsehaud9413@naver.com>
2025-04-09 20:38:55 +0900

    feat: 배포 gh-pages 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,34 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 
-export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.ts",
-      coverage: {
-        reportsDirectory: "./.coverage",
-        reporter: ["lcov", "json", "json-summary"],
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return mergeConfig(
+    defineConfig({
+      plugins: [react()],
+    }),
+    defineTestConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.ts",
+        coverage: {
+          reportsDirectory: "./.coverage",
+          reporter: ["lcov", "json", "json-summary"],
+        },
       },
-    },
-  }),
-);
+      base: env.VITE_BASE_URL,
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, "index.html"),
+            hash: resolve(__dirname, "index.hash.html"),
+          },
+        },
+      },
+    }),
+  );
+};
